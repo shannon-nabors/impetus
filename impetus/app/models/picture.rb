f8279dca636392d58b73f15f25cc56781c8b1ffc
shannon-nabors <shannon.nabors@gmail.com>
2019-07-31 17:55:53 -0400

    added relationships in models

--- impetus/app/models/picture.rb
+++ impetus/app/models/picture.rb
@@ -1,2 +1,4 @@
 class Picture < ApplicationRecord
+    has_many :user_pictures
+    has_many :users, through: :user_pictures
 end
--- impetus/app/models/user.rb
+++ impetus/app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_many :user_quotes
+    has_many :quotes, through: :user_quotes
+    has_many :user_pictures
+    has_many :pictures, through: :user_pictures
+
 end
