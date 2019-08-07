26a494b19d1acb21e87aae0dab3b8da5b1802f59
shannon-nabors <shannon.nabors@gmail.com>
2019-08-07 11:15:03 -0400

    random seeded background pic on page load

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,4 @@
+seededPics = []
 
 document.addEventListener('DOMContentLoaded', init)
 
@@ -15,5 +16,21 @@ function getQuotes() {
 function getPictures() {
     fetch('http://localhost:3000/pictures')
     .then(res => res.json())
-    .then(pictureData => console.log(pictureData))
+    .then(pictureData => {
+        console.log(pictureData)
+        seededPics = pictureData
+        setPicture()
+        // document.body.style.backgroundImage = `url(${pictureData.pictures[0].url})`
+    })
+}
+
+function setPicture() {
+    let pic = randomFromArray(seededPics["pictures"])
+    console.log(`url(${pic.url})`)
+    document.body.style.backgroundImage = `url(${pic.url.split(' ')[2]})`
+    document.body.style.backgroundSize = "cover"
+}
+
+function randomFromArray(items) {
+    return items[Math.floor(Math.random()*items.length)]
 }
